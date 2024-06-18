ec2310ddbde6bd2a313d8e381777974e42c787df
rumiyevskiy <rumiyevskiy@gmail.com>
2024-06-18 14:08:22 +0300

    stylized data side

--- js/script.js
+++ js/script.js
@@ -9,14 +9,40 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 
 const listItems = document.querySelectorAll('.list__item');
 
-const pageElement = document.querySelector('.page__body');
+const pageElement = document.querySelector('.page__field');
 
 listItems.forEach(element => {
     element.addEventListener('click', (event) => {
             event.preventDefault();
             const hrefValue = element.getAttribute('href');
             
-            pageElement.innerHTML = hrefValue;
+            // pageElement.innerHTML = hrefValue;
     }
 );
 });
+
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  console.log('data: ', data);
+  return data;
+};
+
+
+async function main() {
+
+    const customersData = await getData();
+    let currentPage = 1;
+    let rows = 10;
+
+    function displayData (arrData, rowPerPage, page) {
+      
+    };
+
+    function displayPagination () {};
+
+    function displayPaginationBtn () {};
+
+}
+
+main();
