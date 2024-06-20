3860a5ccec9c6b7f201f3e826c4b90cfa267e4d8
rumiyevskiy <rumiyevskiy@gmail.com>
2024-06-20 15:53:42 +0300

    Data page designed and styled

--- js/script.js
+++ js/script.js
@@ -24,7 +24,6 @@ listItems.forEach(element => {
 async function getData() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json();
-  console.log('data: ', data);
   return data;
 };
 
@@ -33,16 +32,180 @@ async function main() {
 
     const customersData = await getData();
     let currentPage = 1;
-    let rows = 10;
+    let rows = 8;
 
     function displayData (arrData, rowPerPage, page) {
+
+      const tableDataEl = document.querySelector('.field__body__table');
+      tableDataEl.innerHTML = '';
+      page--;
+
+      const start = rowPerPage * page;
+      const end = start + rowPerPage;
+      const paginatedData = arrData.slice(start, end);
+
+      // *********************************************
+
+      let paginationDescription = document.querySelector('.field__footer__content__description');
+      let paggDescText = `Showing data ${start+1} to ${start + paginatedData.length} of ${arrData.length} entries`;
+      paginationDescription.innerText = paggDescText;
+
+      // *********************************************
+
+      const tableKeys = ['name', 'company.name', 'phone', 'email', 'country', 'status'];
+      const tableHeaderContent = ['Customer Name', 'Company', 'Phone Number', 'Email', 'Country', 'Status'];
       
+
+      const tableData = document.createElement('table');
+      tableData.setAttribute('class', 'table__data');
+      const thead = document.createElement('thead');
+      const headerRow = document.createElement('tr');
+      tableHeaderContent.forEach(key => {
+        const th = document.createElement('th');
+        th.appendChild(document.createTextNode(`${key}`));
+        headerRow.appendChild(th);
+      });
+      thead.appendChild(headerRow);
+      tableData.appendChild(thead);
+      const tbody = document.createElement('tbody');
+      tableData.appendChild(tbody);
+
+      // *********************************************
+
+      paginatedData.forEach((el) => {
+
+        const tableDataRowEl = tableData.getElementsByTagName('tbody')[0];
+        const newRow = tableDataRowEl.insertRow();
+
+        tableKeys.forEach(key => {
+
+          let keys = key.split('.');
+          let value = keys.reduce((obj, k) => obj && obj[k], el);
+          value = value !== undefined ? value : key;
+          const newCell = newRow.insertCell();
+          const cellBox = document.createElement('div');
+
+          if (key == 'status') {
+            let statusBtn = document.createElement('div');
+            statusBtn.classList.add('status__btn');
+
+            let rndNumber = Math.random() < 0.5 ? 1 : 2;
+
+            if (rndNumber == 2) {
+              statusBtn.classList.add('status__btn--inactive');
+              statusBtn.innerText = 'Inactive';
+            } else {
+              statusBtn.innerText = 'Active';
+            }
+
+            cellBox.appendChild(statusBtn);
+
+          } else {
+            cellBox.appendChild(document.createTextNode(value));
+          };
+          
+          newCell.appendChild(cellBox);
+          
+        });
+
+        tableDataEl.appendChild(tableData);
+
+      })
     };
 
-    function displayPagination () {};
+    function displayPagination (arrData, rowPerPage) {
 
-    function displayPaginationBtn () {};
+      const itemLeft = document.querySelector('.item__left');
+      itemLeft.addEventListener('mousedown', () => {
 
+        itemLeft.classList.add('field__footer__content__pagination__item--active');
+
+        if(currentPage !== 1) {
+
+          currentPage--;
+          displayData(customersData, rows, currentPage);
+          let currentItemLi = document.querySelector('li.field__footer__content__pagination__item--active');
+            currentItemLi.classList.remove('field__footer__content__pagination__item--active');
+          const arrLiEl = document.querySelectorAll('.field__footer__content__pagination__item');
+          arrLiEl[currentPage].classList.add('field__footer__content__pagination__item--active');
+        }
+
+      });
+
+      itemLeft.addEventListener('mouseup', () => {
+        itemLeft.classList.remove('field__footer__content__pagination__item--active');
+      });
+        
+      const itemRight = document.querySelector('.item__right');
+      itemRight.addEventListener('mousedown', () => {
+
+        itemRight.classList.add('field__footer__content__pagination__item--active');
+
+
+
+        if(currentPage !== pagesCount) {
+
+          currentPage++;
+          displayData(customersData, rows, currentPage);
+          let currentItemLi = document.querySelector('li.field__footer__content__pagination__item--active');
+            currentItemLi.classList.remove('field__footer__content__pagination__item--active');
+          const arrLiEl = document.querySelectorAll('.field__footer__content__pagination__item');
+          arrLiEl[currentPage].classList.add('field__footer__content__pagination__item--active');
+        }
+
+      });
+
+      itemRight.addEventListener('mouseup', () => {
+        itemRight.classList.remove('field__footer__content__pagination__item--active');
+      });
+
+      const paginationEl = document.querySelector('.field__footer__content__pagination__content');
+      const pagesCount = Math.ceil(arrData.length / rowPerPage);
+      const ulEl = document.createElement('ul');
+      ulEl.classList.add('field__footer__content__pagination__list');
+
+      for (let i = 0; i < pagesCount; i++) {
+
+        const liEl = displayPaginationBtn(i + 1);
+        ulEl.appendChild(liEl);
+      }
+
+      paginationEl.appendChild(ulEl);
+
+    };
+
+    function displayPaginationBtn (page) {
+
+        const liEl = document.createElement('li');
+        liEl.classList.add('field__footer__content__pagination__item');
+        liEl.innerText = page;
+
+        if (currentPage == page) {
+          liEl.classList.add('field__footer__content__pagination__item--active');
+        }
+
+
+        liEl.addEventListener('click', () => {
+
+          if(currentPage !== page) {
+
+            currentPage = page;
+            displayData(customersData, rows, currentPage);
+
+            let currentItemLi = document.querySelector('li.field__footer__content__pagination__item--active');
+            currentItemLi.classList.remove('field__footer__content__pagination__item--active');
+
+            liEl.classList.add('field__footer__content__pagination__item--active');
+
+          }          
+
+        });
+
+      return liEl;
+    };
+
+    displayData(customersData, rows, currentPage);
+    displayPagination(customersData, rows);
 }
 
 main();
